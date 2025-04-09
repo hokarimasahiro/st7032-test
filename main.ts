760c62358e5f9f2a194631a565cdcebec65a127d
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-04-10 08:37:51 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,43 @@
 st7032.initLcd()
 basic.forever(function () {
     st7032.setPosition(0, 0)
-    for (let counter = 0; counter <= 19; counter++) {
+    st7032.writeData(" ")
+    for (let counter = 0; counter <= 18; counter++) {
+        basic.pause(100)
+        st7032.setPosition(0, counter)
         st7032.writeData(String.fromCharCode(65 + counter))
+        st7032.writeData(" ")
     }
+    st7032.setPosition(0, 19)
+    st7032.writeData(String.fromCharCode(65 + 19))
     st7032.setPosition(1, 0)
-    for (let counter = 0; counter <= 19; counter++) {
+    st7032.writeData(" ")
+    for (let counter = 0; counter <= 18; counter++) {
+        basic.pause(100)
+        st7032.setPosition(1, counter)
         st7032.writeData(String.fromCharCode(97 + counter))
+        st7032.writeData(" ")
     }
+    st7032.setPosition(1, 19)
+    st7032.writeData(String.fromCharCode(97 + 19))
     st7032.setPosition(2, 0)
-    for (let counter = 0; counter <= 19; counter++) {
+    st7032.writeData(" ")
+    for (let counter = 0; counter <= 18; counter++) {
+        basic.pause(100)
+        st7032.setPosition(2, counter)
         st7032.writeData(String.fromCharCode(177 + counter))
+        st7032.writeData(" ")
     }
+    st7032.setPosition(2, 19)
+    st7032.writeData(String.fromCharCode(177 + 19))
     st7032.setPosition(3, 0)
-    for (let counter = 0; counter <= 19; counter++) {
+    st7032.writeData(" ")
+    for (let counter = 0; counter <= 18; counter++) {
+        basic.pause(100)
+        st7032.setPosition(3, counter)
         st7032.writeData(String.fromCharCode(197 + counter))
+        st7032.writeData(" ")
     }
+    st7032.setPosition(3, 19)
+    st7032.writeData(String.fromCharCode(197 + 19))
 })
