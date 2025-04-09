02b74eddba222ba9fff9ffc92f741a4ad750abc2
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-04-10 08:05:43 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
 st7032.initLcd()
 basic.forever(function () {
     st7032.setPosition(0, 0)
-    for (let カウンター = 0; カウンター <= 19; カウンター++) {
-        st7032.writeData(String.fromCharCode(65 + カウンター))
+    for (let counter = 0; counter <= 19; counter++) {
+        st7032.writeData(String.fromCharCode(65 + counter))
     }
     st7032.setPosition(1, 0)
-    for (let カウンター = 0; カウンター <= 19; カウンター++) {
-        st7032.writeData(String.fromCharCode(97 + カウンター))
+    for (let counter = 0; counter <= 19; counter++) {
+        st7032.writeData(String.fromCharCode(97 + counter))
     }
     st7032.setPosition(2, 0)
-    for (let カウンター = 0; カウンター <= 19; カウンター++) {
-        st7032.writeData(String.fromCharCode(177 + カウンター))
+    for (let counter = 0; counter <= 19; counter++) {
+        st7032.writeData(String.fromCharCode(177 + counter))
     }
     st7032.setPosition(3, 0)
-    for (let カウンター = 0; カウンター <= 19; カウンター++) {
-        st7032.writeData(String.fromCharCode(197 + カウンター))
+    for (let counter = 0; counter <= 19; counter++) {
+        st7032.writeData(String.fromCharCode(197 + counter))
     }
 })
