e475b7feaa139f40dca50db403b4de8fc391ead9
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-04-09 17:24:10 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,19 @@
 st7032.initLcd()
 basic.forever(function () {
     st7032.setPosition(0, 0)
-    st7032.writeData("1234567890abcdefg")
+    for (let カウンター = 0; カウンター <= 19; カウンター++) {
+        st7032.writeData(String.fromCharCode(65 + カウンター))
+    }
+    st7032.setPosition(1, 0)
+    for (let カウンター = 0; カウンター <= 19; カウンター++) {
+        st7032.writeData(String.fromCharCode(97 + カウンター))
+    }
+    st7032.setPosition(2, 0)
+    for (let カウンター = 0; カウンター <= 19; カウンター++) {
+        st7032.writeData(String.fromCharCode(177 + カウンター))
+    }
+    st7032.setPosition(3, 0)
+    for (let カウンター = 0; カウンター <= 19; カウンター++) {
+        st7032.writeData(String.fromCharCode(197 + カウンター))
+    }
 })
