d1eec01bd4aff992c4becf3fdae1bdcb767e5cf9
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-04-10 13:01:45 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,34 @@
 basic.forever(function () {
     st7032.setPosition(0, 0)
-    st7032.writeData(" ")
-    for (let counter = 0; counter <= 18; counter++) {
+    st7032.writeData("                    ")
+    st7032.setPosition(0, 0)
+    for (let counter = 0; counter <= 19; counter++) {
         basic.pause(100)
         st7032.setPosition(0, counter)
         st7032.writeData(String.fromCharCode(65 + counter))
-        st7032.writeData(" ")
     }
-    st7032.setPosition(0, 19)
-    st7032.writeData(String.fromCharCode(65 + 19))
     st7032.setPosition(1, 0)
-    st7032.writeData(" ")
-    for (let counter = 0; counter <= 18; counter++) {
+    st7032.writeData("                    ")
+    st7032.setPosition(1, 0)
+    for (let counter2 = 0; counter2 <= 19; counter2++) {
         basic.pause(100)
-        st7032.setPosition(1, counter)
-        st7032.writeData(String.fromCharCode(97 + counter))
-        st7032.writeData(" ")
+        st7032.setPosition(1, counter2)
+        st7032.writeData(String.fromCharCode(97 + counter2))
     }
-    st7032.setPosition(1, 19)
-    st7032.writeData(String.fromCharCode(97 + 19))
     st7032.setPosition(2, 0)
-    st7032.writeData(" ")
-    for (let counter = 0; counter <= 18; counter++) {
+    st7032.writeData("                    ")
+    st7032.setPosition(2, 0)
+    for (let counter3 = 0; counter3 <= 19; counter3++) {
         basic.pause(100)
-        st7032.setPosition(2, counter)
-        st7032.writeData(String.fromCharCode(177 + counter))
-        st7032.writeData(" ")
+        st7032.setPosition(2, counter3)
+        st7032.writeData(String.fromCharCode(177 + counter3))
     }
-    st7032.setPosition(2, 19)
-    st7032.writeData(String.fromCharCode(177 + 19))
     st7032.setPosition(3, 0)
-    st7032.writeData(" ")
-    for (let counter = 0; counter <= 18; counter++) {
+    st7032.writeData("                    ")
+    st7032.setPosition(3, 0)
+    for (let counter4 = 0; counter4 <= 19; counter4++) {
         basic.pause(100)
-        st7032.setPosition(3, counter)
-        st7032.writeData(String.fromCharCode(197 + counter))
-        st7032.writeData(" ")
+        st7032.setPosition(3, counter4)
+        st7032.writeData(String.fromCharCode(197 + counter4))
     }
-    st7032.setPosition(3, 19)
-    st7032.writeData(String.fromCharCode(197 + 19))
 })
